9ff1105f37fcf5bc3cfc8bef5e8f3aa506ac0253
Nejc Bevk <nejc.bevk@tronog.com>
2025-11-24 23:25:37 +0100

    Image card update + download drag

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,12 +1,39 @@
 <script setup lang="ts">
 import type { Img } from '../stores/brains'
+import { useBrainsStore } from '../stores/brains'
+import { saveAs } from 'file-saver'
 
 const props = defineProps<{ photo: Img }>()
+const store = useBrainsStore()
+
+const startDrag = (e: DragEvent) => {
+  if (!e.dataTransfer) return
+  const url = props.photo.src.original
+  const filename = `${props.photo.alt.replace(/[^a-z0-9]/gi, '_')}_${props.photo.id}.jpg`
+  e.dataTransfer.setData('DownloadURL', `image/jpeg:${filename}:${url}`)
+}
+
+const download = async () => {
+  const blob = await fetch(props.photo.src.original).then(r => r.blob())
+  console.log("ImageCard: blob", blob)
+  console.log("ImageCard: props", props.photo)
+  saveAs(blob, `${props.photo.alt}_${props.photo.id}.jpg`)
+  
+}
 </script>
 
 <template>
-  <div>
+  <div
+    class="group relative rounded-xl overflow-hidden shadow-2xl cursor-grab active:cursor-grabbing transition-all hover:scale-[1.02]"
+    draggable="true" @dragstart="startDrag" @click="download">
     <img :src="photo.src.large2x" :alt="photo.alt" class="w-full aspect-video object-cover" loading="lazy" />
-    
+
+    <div class="absolute inset-0 bg-gradient-to-t from-black/90 opacity-0 group-hover:opacity-100 transition">
+      <div class="absolute bottom-4 left-4 right-4 text-white">
+        <p class="font-bold text-lg truncate">{{ photo.alt }}</p>
+        <p class="text-sm opacity-80">by {{ photo.photographer }}</p>
+        <p class="text-xs mt-2 opacity-70">Click to download | Drag to desktop/Figma</p>
+      </div>
+    </div>
   </div>
 </template>
